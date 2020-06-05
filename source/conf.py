05630bd20d080efb7ff914c07c04328b157a9db1
zhou xy <zhouxy@zhoudeMacBook-Pro.local>
2020-06-05 17:17:34 +0800

    fix theme

--- source/conf.py
+++ source/conf.py
@@ -55,9 +55,9 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
+#html_theme = 'sphinx_rtd_theme'
 
-#import sphinx_ustack_theme
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_ustack_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
